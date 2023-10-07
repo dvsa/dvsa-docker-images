4d00b048e1726235419e25b7a890233498a237ba
Rahul Sharma <sharmcra@amazon.co.uk>
2023-10-07 23:05:54 +0100

    [awsreset-365]: updated workflow

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,19 +1,19 @@
 SHA=$(git rev-parse --short HEAD)
-for results in $(jq -c '.[]' build.json); do
 
-    repoName=$(echo "results" | jq -r '.repoName') 
-    dockerFile=$(echo "results" | jq -r '.dockerFile') 
-    tag=$(echo "results" | jq -r '.tag') 
-    build=$(echo "results" | jq -r '.build')
-    registry=$(echo "results" | jq -r '.registry')
+# Iterate over JSON objects in build.json
+jq -c '.[]' build.json | while read -r results; do
 
+    repoName=$(echo "$results" | jq -r '.repoName') 
+    dockerFile=$(echo "$results" | jq -r '.dockerFile') 
+    tag=$(echo "$results" | jq -r '.tag') 
+    build=$(echo "$results" | jq -r '.build')
+    registry=$(echo "$results" | jq -r '.registry')
 
     if [ "$build" == "true" ]; then
         echo "Building $repoName ..."
-        buildTag="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$repo/$repoName:$tag-$SHA
-        docker build -t $buildTag --file ./build/$repoName/$dockerFile build/.
+        buildTag="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$repo/$repoName:$tag-$SHA"
+        docker build -t "$buildTag" --file "./build/$repoName/$dockerFile" build/.
     else
-        echo "Not building - $repoName ,build parameter equal to false"
+        echo "Not building - $repoName, build parameter equal to false"
     fi
-
 done
