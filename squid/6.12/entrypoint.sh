a652e30e4898f48b6d55e6f7b19478a10de1e9f6
Lewis Brown <Lewis.Brown@m-gdt6n2nm91.fritz.box>
2025-01-03 15:07:44 +0000

    clean up

--- squid/6.12/entrypoint.sh
+++ squid/6.12/entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
   if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
     echo "Initialising cache..."
-    echo ${SQUID_CACHE_DIR}
     squid -N -f /etc/squid/conf.d/squid.conf -z
   fi
   echo "Starting squid..."
