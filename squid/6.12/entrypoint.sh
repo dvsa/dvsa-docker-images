c3ed91d3f9d7a0a51ed1ccd4380b336d330a7d54
Lukasz Chlipala <lukasz.chlipala@kainos.com>
2025-02-07 11:48:35 +0100

    chore: deploy test

--- squid/6.12/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-  if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
-    echo "Initialising cache..."
-    squid -N -f /etc/squid/conf.d/squid.conf -z
-  fi
-  echo "Starting squid..."
-  squid -NYC -f /etc/squid/conf.d/squid.conf
